dd9b4e8675ee7ece41daacebaf20bb076f495ba1
Astivero-x3d <ismaelast2005@gmail.com>
2024-01-10 10:39:44 +0100

    Commit: Añadir P4.java

---
+++ P4/src/main/java/buclewhile/p4/P4.java
@@ -0,0 +1,13 @@
+package buclewhile.p4;
+
+/**
+ *
+ * @author Ismael_Ast
+ */
+public class P4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("HEY");
+    }
+}
