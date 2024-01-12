c41869cbe6f0135576154fedcdb5b2885864fc26
Astivero-x3d <ismaelast2005@gmail.com>
2024-01-12 11:45:14 +0100

    Commit: Añadir P4.java y pom.xl

--- P4/src/main/java/buclewhile/p4/P4.java
+++ P4/src/main/java/buclewhile/p4/P4.java
@@ -9,5 +9,6 @@ public class P4 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("HEY");
+        System.out.println("HEY2");
     }
 }
